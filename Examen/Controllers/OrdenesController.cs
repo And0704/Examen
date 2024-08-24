f2d56005b906722905ea6ea5c19b27516dee23fb
Josue Castillo <josuecastillo254@gmail.com>
2024-08-24 14:48:20 -0500

    Puesto la base del fronedn

---
+++ Examen/Controllers/OrdenesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen.Controllers
+{
+    public class OrdenesController : Controller
+    {
+        // GET: Ordenes
+        public ActionResult ordenCompra()
+        {
+            return View();
+        }
+    }
+}
