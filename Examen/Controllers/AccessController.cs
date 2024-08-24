9f80111c1ebf81b8f3b1e47c9f5509cd75e42b6f
Kiddozepam <uleanez@gmail.com>
2024-08-24 15:03:37 -0500

    Creación de Controlador Acceso y Vistas

---
+++ Examen/Controllers/AccessController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+    }
+}
