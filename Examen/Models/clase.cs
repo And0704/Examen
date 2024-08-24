7ad2d2be75a25c9b513e3c9b6e47d79b87163b75
Josue Castillo <josuecastillo254@gmail.com>
2024-08-24 15:38:18 -0500

    Inicio de modelo

---
+++ Examen/Models/clase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen.Models
+{
+    public class obtenerInformacion
+    {
+        public int product_id { get; set; }
+        public string product_name { get; set; }
+
+        public 
+    }
+}
